c39c084b7f74e73b10b1b60f725bcc5bdec84e66
someone5678 <nemui3353@gmail.com>
2023-07-28 12:26:09 +0900

    [TREEWIDE] Redirect all links to this account
    Change-Id: Icc41aa54ba2e57c78905b3314e8bb8d7aeab8df8

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ Possible causes of error:
     pkg install git -y -o Dpkg::Options::="--force-confnew"
 fi
 
-if git clone --depth=1 https://github.com/decipher3114/Revancify.git; then
+if git clone --depth=1 https://github.com/someone5678/Revancify.git; then
     $HOME/Revancify/revancify
 else
     echo -e "\e[1;31mInstall Failed !!\e[0m"
