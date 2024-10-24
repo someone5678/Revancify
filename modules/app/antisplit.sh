f60c2e08faf210bbd12dd0b28b46280a5ef70148
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-24 11:33:54 +0900

    fix: Don't reduce split apk
    * Required for translation patch
    * Simplify it
    
    Change-Id: Ia14be83931eb4d6d53a356e76f981c8577e48934

--- modules/app/antisplit.sh
+++ modules/app/antisplit.sh
@@ -3,24 +3,11 @@
 antisplitApp() {
     local SPLITS APP_DIR LOCALE
 
-    notify info "Please Wait !!\nReducing app size..."
+    notify info "Please Wait !!\nMerging split apks..."
 
-    APP_DIR="apps/$APP_NAME/$APP_VER"
-    if [ ! -e "$APP_DIR" ]; then
-        LOCALE=$(getprop persist.sys.locale | sed 's/-.*//g')
-        if [ ! -e "$SPLITS/split_config.${LOCALE}.apk" ]; then
-            LOCALE=$(getprop ro.product.locale | sed 's/-.*//g')
-        fi
-        unzip -qqo \
-            "apps/$APP_NAME/$APP_VER.apkm" \
-            "base.apk"\
-            "split_config.${ARCH//-/_}.apk" \
-            "split_config.${LOCALE}.apk" \
-            split_config.*dpi.apk \
-            -d "$APP_DIR"
-        rm "apps/$APP_NAME/$APP_VER.apkm"
-    fi
-    java -jar bin/APKEditor.jar m -i "$APP_DIR" -o "apps/$APP_NAME/$APP_VER.apk" &> /dev/null
+    unzip -qqo "apps/$APP_NAME/$APP_VER.apkm" -d "$APP_DIR"
+    rm "apps/$APP_NAME/$APP_VER.apkm"
+    java -jar bin/ApkEditor.jar m -i "$APP_DIR" -o "apps/$APP_NAME/$APP_VER.apk" &> /dev/null
     if [ ! -e "apps/$APP_NAME/$APP_VER.apk" ]; then
         notify msg "Unable to run merge splits!!\nApkEditor is not working properly."
         return 1
